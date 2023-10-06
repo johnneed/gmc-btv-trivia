b4da528de9a4c1f4845e1dcae059e4ae6ba013b4
John Need <john.need@inulabs.tech>
2023-10-06 08:43:25 -0400

    fix: unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders the h1 element", () => {
     render(<App/>);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const titleElement = screen.getByText(/GMC-BTV Trail Trivia/i);
+    expect(titleElement).toBeInTheDocument();
 });
